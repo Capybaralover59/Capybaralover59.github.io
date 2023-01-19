6c7b4fc8b0028218ccd47f0ef55bb42711b1fae6
Capybaralover59 <casdel@edu.umea.se>
2023-01-19 12:25:13 +0100

    done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,22 @@
     clicks = clicks + multiplier;
     console.log("click" + clicks);
   }
-
+  function nFormatter(num, digits) {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "G" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" }
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  var item = lookup.slice().reverse().find(function(item) {
+    return num >= item.value;
+  });
+  return item ? (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol : "0";
+}
   import AudioPlayer, { stopAll } from '$lib/components/AudioPlayer.svelte'
 
 let audioTracks = [
@@ -65,8 +80,8 @@ let audioTracks = [
   </header>
   <div class="game">
     <button on:click={increment} class="clicker">
-      <span class="clicks">🌙{clicks}</span>
-      <span class="pointtext">:MT {multiplier}</span>
+      <span class="clicks">🌙{nFormatter(clicks)}</span>
+      <span class="pointtext">:MT {nFormatter(multiplier)}</span>
     </button>
   </div>
   <footer>
